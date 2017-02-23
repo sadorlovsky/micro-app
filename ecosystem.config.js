de5e3cbad1f5eb72fdb426e582f4693e9dc27e57
Zach Orlovsky <sadorlovsky@gmail.com>
2017-02-23 03:56:50 +0300

    native async/await

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,8 +3,6 @@ module.exports = {
     {
       name: 'micro-app',
       script: 'node_modules/.bin/micro',
-      args: 'server.js',
-      node_args: '--harmony-async-await',
       exec_mode: 'cluster',
       instances: 4
     }
